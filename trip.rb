1a70eaca113a79eb9d2b1157ac75c17291b60cea
Jaden <jcottorone21405@gmail.com>
2018-06-04 20:55:30 +0000

    Done.

--- trip.rb
+++ trip.rb
@@ -1,9 +1,9 @@
 puts "Where would you like to stay?"
-stay=gets.capitalize
+stay=gets.capitalize.strip
 puts "Where would you like to visit?"
-site=gets
+site=gets.strip
 puts "What would you like to eat?"
-eat=gets.capitalize
+eat=gets.capitalize.strip
 puts "How many nights do you want to stay?"
-nights=gets.to_i
+nights=gets.to_i.strip
 puts "This is your plan for your trip: You want to stay at #{stay},visit #{site}, eat some #{eat} and stay #{nights} nights"
